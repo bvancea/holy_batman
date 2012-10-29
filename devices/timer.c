68d9872f692b8c8d2f5758cce94c31b22517e600
Operating System Desing <osd@osd-project.(none)>
2012-10-29 21:42:51 +0200

    Removed annoying thing that made running pintos pointless

--- devices/timer.c
+++ devices/timer.c
@@ -172,7 +172,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
-  printf("Time is %d\n", ticks);
+  //printf("Time is %d\n", ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
